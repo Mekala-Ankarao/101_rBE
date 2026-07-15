ff7b9386556841556904f46d8cc971f9e2c2a8b7
Mekala-Ankarao <mekalaankarao1@gmail.com>
2026-07-15 21:08:22 +0530

    Ugly Number

--- src/com/uglyNumber/Example1.java
+++ src/com/uglyNumber/Example1.java
@@ -1,10 +1,24 @@
 package com.uglyNumber;
 
-public class Example1 {
+public class Example1 { 
+	public boolean isUgly(int num) {
+		if(num<=0) {
+		}
+		while(num%2==0) {
+			num/=2;
+		}
+		while(num%3==0) {
+			num/=3;
+		}
+		while(num%5==0) {
+			num/=5;
+		}
+		return num == 1;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Example1 obj = new Example1();
+		System.out.println(obj.isUgly(7)?"it is Ugly Number":"it is Not Ugly Number");
 	}
 
 }
